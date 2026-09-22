aa9443e3d42b86093d2fc297efcc5db0992b3d39
DiegoMiguel04 <230260@utxicotepec.edu.mx>
2026-09-21 18:24:36 -0600

    Practica03
    Avances Practica03

--- Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../widgets/chat/my_message_bubble.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/chat_provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -8,18 +13,17 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.all(4.0),
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUBf1YpZy9ejF0Jp6xTi-0K7s_gbh7DbRqLjGxtZ9yOw&s=10"
-            ),
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUBf1YpZy9ejF0Jp6xTi-0K7s_gbh7DbRqLjGxtZ9yOw&s=10'),
           ),
         ),
-        title: const Text("Gato"),
+        title: const Text('El Gato'),
         centerTitle: false,
       ),
-      body: _ChatView()
+      body: _ChatView(),
     );
   }
 }
@@ -27,20 +31,28 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return const MyMessageBubble();
-              }
-            )),
-        
-            
-            Text("Hola mundo"),
+            Expanded(
+              child: ListView.builder(
+                controller: chatProvider.chatScrollController,
+                itemCount: chatProvider.messageList.length,
+                itemBuilder: (context, index) {
+                  final message = chatProvider.messageList[index];
+                  return (message.fromWho == FromWho.her)
+                    ? const HerMessageBubble()
+                    : MyMessageBubble(message: message);
+                }
+              )
+            ),/// Caja de texto de mensajes
+            MessageFieldBox(
+              onValue: chatProvider.sendMessage,
+            ),
           ],
         ),
       ),
--- Practica03/yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
+++ Practica03/yes_no_app/lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({super.key});
+  final Message message;
+
+  const MyMessageBubble({
+    super.key,
+    required this.message
+  });
 
   @override
   Widget build(BuildContext context) {
+
     final colors = Theme.of(context).colorScheme;
 
     return Column(
@@ -12,18 +19,16 @@ class MyMessageBubble extends StatelessWidget {
       children: [
         Container(
           decoration: BoxDecoration(
-            color: colors.primary,
-            borderRadius: BorderRadius.circular(20),
-          ),
+              color: colors.primary, borderRadius: BorderRadius.circular(20)),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: const Text(
-              "Hola mundo",
-              style: TextStyle(color: Colors.white),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 10)
       ],
     );
   }
